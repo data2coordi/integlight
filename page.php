a3c6d4f95bde05d03bfda9a65898c7bb0b73d17f
data2coordi <data2coordi@gmail.com>
2024-06-22 04:19:43 +0000

    slideをテンプレートパーツに変更

--- page.php
+++ page.php
@@ -24,49 +24,13 @@ get_header();
 	// パンくず_e //////////////////
 	?>
 
-
-	<!-- slide bar _s //////////////////////////////// -->
 	<?php
+	// slide 
 	if (!is_home() && is_front_page()) :
-		$slider_image_1 = get_theme_mod('slider_image_1');
-		$slider_image_2 = get_theme_mod('slider_image_2');
-		$slider_image_3 = get_theme_mod('slider_image_3');
-		if (!empty($slider_image_1) || !empty($slider_image_2) || !empty($slider_image_3)) :
+		get_template_part('template-parts/content', 'slide');
+	endif
 	?>
-			<div class="slider">
-				<div class="slides">
 
-					<!-- slide 1 -->
-					<?php if (!empty($slider_image_1)) : ?>
-						<div class="slide">
-							<img src="<?php echo esc_url($slider_image_1); ?>" alt="Slide 1">
-						</div>
-					<?php endif; ?>
-
-					<!-- slide 2 -->
-					<?php if (!empty($slider_image_2)) : ?>
-						<div class="slide">
-							<img src="<?php echo esc_url($slider_image_2); ?>" alt="Slide 2">
-						</div>
-					<?php endif; ?>
-
-					<!-- slide 3 -->
-					<?php if (!empty($slider_image_3)) : ?>
-						<div class="slide">
-							<img src="<?php echo esc_url($slider_image_3); ?>" alt="Slide 3">
-						</div>
-					<?php endif; ?>
-
-					<div class="text-overlay">
-						<p><?php echo nl2br(wp_kses_post(get_theme_mod('slider_text_1', ''))); ?></p>
-					</div>
-				</div>
-			</div>
-	<?php
-		endif;
-	endif;
-	?>
-	<!-- slide bar _e //////////////////////////////// -->
 
 
 	<?php
---
+++ template-parts/content-slide.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Template part for displaying slide on top page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Integlight
+ */
+
+?>
+
+<!-- slide bar _s //////////////////////////////// -->
+<?php
+$slider_image_1 = get_theme_mod('slider_image_1');
+$slider_image_2 = get_theme_mod('slider_image_2');
+$slider_image_3 = get_theme_mod('slider_image_3');
+if (!empty($slider_image_1) || !empty($slider_image_2) || !empty($slider_image_3)) :
+?>
+	<div class="slider">
+		<div class="slides">
+
+			<!-- slide 1 -->
+			<?php if (!empty($slider_image_1)) : ?>
+				<div class="slide">
+					<img src="<?php echo esc_url($slider_image_1); ?>" alt="Slide 1">
+				</div>
+			<?php endif; ?>
+
+			<!-- slide 2 -->
+			<?php if (!empty($slider_image_2)) : ?>
+				<div class="slide">
+					<img src="<?php echo esc_url($slider_image_2); ?>" alt="Slide 2">
+				</div>
+			<?php endif; ?>
+
+			<!-- slide 3 -->
+			<?php if (!empty($slider_image_3)) : ?>
+				<div class="slide">
+					<img src="<?php echo esc_url($slider_image_3); ?>" alt="Slide 3">
+				</div>
+			<?php endif; ?>
+
+			<div class="text-overlay">
+				<p><?php echo nl2br(wp_kses_post(get_theme_mod('slider_text_1', ''))); ?></p>
+			</div>
+		</div>
+	</div>
+<?php
+endif;
+?>
+<!-- slide bar _e //////////////////////////////// -->
