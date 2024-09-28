c9257d0126e1718960c6ce7355c6799a469d492e
data2coordi <data2coordi@gmail.com>
2024-09-28 01:59:41 +0000

    リファクタリング：CSS全体

--- integlight-functions.php
+++ integlight-functions.php
@@ -19,6 +19,7 @@ function integlight_scripts_plus()
 	wp_enqueue_style('integlight-style-plus', get_template_directory_uri() . '/integlight-style.css', array(), _S_VERSION);
 	//wp_enqueue_script('integlight-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 	wp_enqueue_style('integlight-post', get_template_directory_uri() . '/css/post.css', array(), _S_VERSION);
+	wp_enqueue_style('integlight-page', get_template_directory_uri() . '/css/page.css', array(), _S_VERSION);
 
 	//web fonts: font awsome
 	wp_enqueue_style('integlight-owsome', get_template_directory_uri() . '/css/all.min.css', array(), _S_VERSION);
