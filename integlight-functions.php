02b127dc612993dab3f7a3003bf2765987117125
data2coordi <data2coordi@gmail.com>
2024-10-06 07:58:13 +0000

    backstopjs導入直後

--- integlight-functions.php
+++ integlight-functions.php
@@ -18,8 +18,12 @@ function integlight_scripts_plus()
 
 	wp_enqueue_style('integlight-style-plus', get_template_directory_uri() . '/integlight-style.css', array(), _S_VERSION);
 	//wp_enqueue_script('integlight-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+	wp_enqueue_style('integlight-layout', get_template_directory_uri() . '/css/layout.css', array(), _S_VERSION);
 	wp_enqueue_style('integlight-post', get_template_directory_uri() . '/css/post.css', array(), _S_VERSION);
 	wp_enqueue_style('integlight-page', get_template_directory_uri() . '/css/page.css', array(), _S_VERSION);
+	wp_enqueue_style('integlight-front', get_template_directory_uri() . '/css/front.css', array(), _S_VERSION);
+	wp_enqueue_style('integlight-home', get_template_directory_uri() . '/css/home.css', array(), _S_VERSION);
+	wp_enqueue_style('integlight-module', get_template_directory_uri() . '/css/module.css', array(), _S_VERSION);
 
 	//web fonts: font awsome
 	wp_enqueue_style('integlight-owsome', get_template_directory_uri() . '/css/all.min.css', array(), _S_VERSION);
