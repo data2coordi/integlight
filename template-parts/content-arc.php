f43a56b5c912b6ac07098f808031346dc0def27b
data2coordi <data2coordi@gmail.com>
2025-02-05 13:48:45 +0000

    カテゴリー一覧をグリッドに変更

--- template-parts/content-arc.php
+++ template-parts/content-arc.php
@@ -11,10 +11,9 @@
 
 <div class="bl_card_container">
 	<!-- 左側のカラム：画像 -->
-	<div class="bl_card_column">
+	<div class="bl_card_img">
 		<a href="<?php the_permalink(); ?>" class="bl_card_link">
 			<figure class="bl_card_imgWrapper">
-				<img src="<?php the_post_thumbnail_url(); ?>" alt="">
 				<?php
 				$categories = get_the_category();
 				if (!empty($categories)) {
@@ -22,22 +21,20 @@
 					echo '<span class="category-label">' . esc_html($last_category->name) . '</span>';
 				}
 				?>
+				<img src="<?php the_post_thumbnail_url(); ?>" alt="">
 			</figure>
 		</a>
 	</div>
 
 	<!-- 右側のカラム：本文 -->
-	<div class="bl_card_column">
-		<article class="bl_card">
+	<div class="bl_card_head">
 
-			<a href="<?php the_permalink(); ?>">
-				<?php the_title('<h5 class="bl_card_ttl">', '</h5>'); ?>
-			</a>
-			<div class="bl_card_body">
-				<span class="entry-date"><i class="fa-solid fa-calendar-days"></i><?php echo get_the_date(); ?></span>
-				<p class="bl_card_txt"><?php echo get_the_excerpt(); ?></p>
-				<a href="<?php the_permalink(); ?>"><i class="fa-solid fa-up-right-from-square"></i> continue</a>
-			</div>
-		</article>
+		<a href="<?php the_permalink(); ?>">
+			<?php the_title('<h5 class="bl_card_ttl">', '</h5>'); ?>
+		</a>
+		<span class="entry-date"><i class="fa-solid fa-calendar-days"></i><?php echo get_the_date(); ?></span>
+	</div>
+	<div class="bl_card_body">
+		<p class="bl_card_txt"><?php echo get_the_excerpt(); ?></p>
 	</div>
 </div>
