6f43f72521d0551c269bbbf0eb80678dbc86ac78
data2coordi <data2coordi@gmail.com>
2025-02-16 09:09:45 +0000

    カスタムカバー暫定版完成

--- blocks/custom-cover/build/index.asset.php
+++ blocks/custom-cover/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a783864e95f7136db383');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bf1faba5b1fe5cb0a097');
