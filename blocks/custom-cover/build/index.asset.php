603e86e7cdae788eaafacfbd993a217ba40484ea
data2coordi <data2coordi@gmail.com>
2025-02-17 06:49:41 +0000

    調整

--- blocks/custom-cover/build/index.asset.php
+++ blocks/custom-cover/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c1bd0aa22f22cd0491be');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '817717aba440fe478905');
