9a165dfc2f86093310382e799c8d52407d1b7a8f
data2coordi <data2coordi@gmail.com>
2025-02-18 10:00:41 +0000

    吹き出し調整

--- blocks/speech-bubble/build/index.asset.php
+++ blocks/speech-bubble/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '9d8a72d4865281f8cd68');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '3e5e64fdb4b97fd15281');
