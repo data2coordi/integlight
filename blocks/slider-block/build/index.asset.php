661ce6e5fa74a581fef535584b1bd56edb147ae9
data2coordi <data2coordi@gmail.com>
2025-02-23 09:24:12 +0000

    pattern２ボタン色

--- blocks/slider-block/build/index.asset.php
+++ blocks/slider-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'b82fde9bead6a32a0e4d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0e08f8d9be862b36c411');
