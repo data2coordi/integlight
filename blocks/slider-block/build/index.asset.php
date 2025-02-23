bff5683fd7b343b66b323961202a686488848c93
data2coordi <data2coordi@gmail.com>
2025-02-23 01:29:11 +0000

    コンテンツスライダーの編集画面のCSS変更

--- blocks/slider-block/build/index.asset.php
+++ blocks/slider-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'a27359628e910c58074f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'b82fde9bead6a32a0e4d');
