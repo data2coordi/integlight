dd7fedc5591f71d41866e5db22776d36f93c3a08
data2coordi <data2coordi@gmail.com>
2025-04-13 21:38:08 +0900

    サイドバーの初期値が設定されない問題を修正

--- sidebar.php
+++ sidebar.php
@@ -17,9 +17,9 @@ if (is_page()) {
 
 
 if (is_active_sidebar('sidebar-1')) {
-	if (get_theme_mod('integlight_sidebar1_position') !== 'none') {
+	if (get_theme_mod('integlight_sidebar1_position', 'right') !== 'none') {
 ?>
-		<aside id="secondary" class="widget-area <?php echo esc_attr("ly_site_content_widgetArea_" . get_theme_mod('integlight_sidebar1_position')); ?>">
+		<aside id="secondary" class="widget-area <?php echo esc_attr("ly_site_content_widgetArea_" . get_theme_mod('integlight_sidebar1_position', 'right')); ?>">
 
 
 			<?php dynamic_sidebar('sidebar-1'); ?>
@@ -31,9 +31,9 @@ if (is_active_sidebar('sidebar-1')) {
 
 <?php
 if (is_active_sidebar('sidebar-2')) {
-	if (get_theme_mod('integlight_sidebar2_position') !== 'none') {
+	if (get_theme_mod('integlight_sidebar2_position', 'none') !== 'none') {
 ?>
-		<aside id="third" class="widget-area <?php echo esc_attr("ly_site_content_widgetArea_" . get_theme_mod('integlight_sidebar2_position'));  ?>">
+		<aside id="third" class="widget-area <?php echo esc_attr("ly_site_content_widgetArea_" . get_theme_mod('integlight_sidebar2_position', 'none'));  ?>">
 
 
 			<?php dynamic_sidebar('sidebar-2'); ?>
