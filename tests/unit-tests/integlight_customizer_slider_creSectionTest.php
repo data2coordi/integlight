9ae572f4b9aa928178cf4a5496fb4d8396708821
data2coordi <data2coordi@gmail.com>
2025-04-19 19:11:16 +0900

    最近の投稿のインデントを取る

--- tests/unit-tests/integlight_customizer_slider_creSectionTest.php
+++ tests/unit-tests/integlight_customizer_slider_creSectionTest.php
@@ -1,41 +1,46 @@
-<?php
+                <?php
 
-class integlight_customizer_slider_creSectionTest extends  WP_UnitTestCase
-{
-    public function testConstants()
-    {
-        $instance = new integlight_customizer_slider_creSection();
+                class integlight_customizer_slider_creSectionTest extends  WP_UnitTestCase
+                {
+                    public function testConstants()
+                    {
+                        $instance = new integlight_customizer_slider_creSection();
 
-        $this->assertSame('slider_panel', $instance->getSliderPanelId());
-        $this->assertSame('slider_section', $instance->getSliderSectionId());
-        $this->assertSame('sliderOrImage_section', $instance->getSliderOrImageSectionId());
-    }
+                        $this->assertSame('slider_panel', $instance->getSliderPanelId());
+                        $this->assertSame('slider_section', $instance->getSliderSectionId());
+                        $this->assertSame('sliderOrImage_section', $instance->getSliderOrImageSectionId());
+                    }
 
-    public function testCustomizeRegisterHook()
-    {
-        // add_action が正しく呼ばれているかは WordPress の環境が必要ですが、
-        // 通常は do_action('customize_register') で creSection が呼ばれることで間接的に確認します。
-        // ここでは簡易的に add_action のテストを飛ばし、creSection の挙動に注目します。
-        $instance = new integlight_customizer_slider_creSection();
+                    public function testCustomizeRegisterHook()
+                    {
 
-        $mock_customize = $this->createMock(WP_Customize_Manager::class);
 
-        // add_panel の引数を検証
-        $mock_customize->expects($this->once())
-            ->method('add_panel')
-            ->with(
-                $this->equalTo('slider_panel'),
-                $this->callback(function ($args) {
-                    return $args['title'] === 'Top Header Setting' &&
-                        isset($args['description']) &&
-                        $args['priority'] === 29;
-                })
-            );
+                        if (!class_exists('WP_Customize_Manager')) {
+                            require_once ABSPATH . WPINC . '/class-wp-customize-manager.php';
+                        }
+                        // add_action が正しく呼ばれているかは WordPress の環境が必要ですが、
+                        // 通常は do_action('customize_register') で creSection が呼ばれることで間接的に確認します。
+                        // ここでは簡易的に add_action のテストを飛ばし、creSection の挙動に注目します。
+                        $instance = new integlight_customizer_slider_creSection();
 
-        // add_section は2回呼ばれるので、呼び出し回数を指定
-        $mock_customize->expects($this->exactly(2))
-            ->method('add_section');
+                        $mock_customize = $this->createMock(WP_Customize_Manager::class);
 
-        $instance->creSection($mock_customize);
-    }
-}
+                        // add_panel の引数を検証
+                        $mock_customize->expects($this->once())
+                            ->method('add_panel')
+                            ->with(
+                                $this->equalTo('slider_panel'),
+                                $this->callback(function ($args) {
+                                    return $args['title'] === 'Top Header Setting' &&
+                                        isset($args['description']) &&
+                                        $args['priority'] === 29;
+                                })
+                            );
+
+                        // add_section は2回呼ばれるので、呼び出し回数を指定
+                        $mock_customize->expects($this->exactly(2))
+                            ->method('add_section');
+
+                        $instance->creSection($mock_customize);
+                    }
+                }
