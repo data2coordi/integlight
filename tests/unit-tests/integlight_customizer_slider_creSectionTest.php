a211d91e46c8ea9d992dd64a35037ff2210b4481
data2coordi <data2coordi@gmail.com>
2025-04-19 18:59:59 +0900

    最近の投稿のインデントを取る

--- tests/unit-tests/integlight_customizer_slider_creSectionTest.php
+++ tests/unit-tests/integlight_customizer_slider_creSectionTest.php
@@ -1,41 +1,41 @@
 <?php
 
-class integlight_customizer_slider_creSectionTest extends extends WP_UnitTestCase
+class integlight_customizer_slider_creSectionTest extends  WP_UnitTestCase
 {
-public function testConstants()
-{
-$instance = new integlight_customizer_slider_creSection();
+    public function testConstants()
+    {
+        $instance = new integlight_customizer_slider_creSection();
 
-$this->assertSame('slider_panel', $instance->getSliderPanelId());
-$this->assertSame('slider_section', $instance->getSliderSectionId());
-$this->assertSame('sliderOrImage_section', $instance->getSliderOrImageSectionId());
-}
+        $this->assertSame('slider_panel', $instance->getSliderPanelId());
+        $this->assertSame('slider_section', $instance->getSliderSectionId());
+        $this->assertSame('sliderOrImage_section', $instance->getSliderOrImageSectionId());
+    }
 
-public function testCustomizeRegisterHook()
-{
-// add_action が正しく呼ばれているかは WordPress の環境が必要ですが、
-// 通常は do_action('customize_register') で creSection が呼ばれることで間接的に確認します。
-// ここでは簡易的に add_action のテストを飛ばし、creSection の挙動に注目します。
-$instance = new integlight_customizer_slider_creSection();
+    public function testCustomizeRegisterHook()
+    {
+        // add_action が正しく呼ばれているかは WordPress の環境が必要ですが、
+        // 通常は do_action('customize_register') で creSection が呼ばれることで間接的に確認します。
+        // ここでは簡易的に add_action のテストを飛ばし、creSection の挙動に注目します。
+        $instance = new integlight_customizer_slider_creSection();
 
-$mock_customize = $this->createMock(WP_Customize_Manager::class);
+        $mock_customize = $this->createMock(WP_Customize_Manager::class);
 
-// add_panel の引数を検証
-$mock_customize->expects($this->once())
-->method('add_panel')
-->with(
-$this->equalTo('slider_panel'),
-$this->callback(function($args) {
-return $args['title'] === 'Top Header Setting' &&
-isset($args['description']) &&
-$args['priority'] === 29;
-})
-);
+        // add_panel の引数を検証
+        $mock_customize->expects($this->once())
+            ->method('add_panel')
+            ->with(
+                $this->equalTo('slider_panel'),
+                $this->callback(function ($args) {
+                    return $args['title'] === 'Top Header Setting' &&
+                        isset($args['description']) &&
+                        $args['priority'] === 29;
+                })
+            );
 
-// add_section は2回呼ばれるので、呼び出し回数を指定
-$mock_customize->expects($this->exactly(2))
-->method('add_section');
+        // add_section は2回呼ばれるので、呼び出し回数を指定
+        $mock_customize->expects($this->exactly(2))
+            ->method('add_section');
 
-$instance->creSection($mock_customize);
-}
+        $instance->creSection($mock_customize);
+    }
 }
