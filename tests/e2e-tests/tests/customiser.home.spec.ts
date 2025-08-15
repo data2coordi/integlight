5ffcfcf74804d07792d54e414925eca37a482751
data2coordi <data2coordi@gmail.com>
2025-08-15 13:58:33 +0900

    tests/e2e-tests/tests/customiser.home.spec.ts 完成

--- tests/e2e-tests/tests/customiser.home.spec.ts
+++ tests/e2e-tests/tests/customiser.home.spec.ts
@@ -34,7 +34,7 @@ async function setSiteType(page, siteType = 'エレガント') {
     // すでにチェックされていれば何もしない
     if (!(await checkbox.isChecked())) {
         await checkbox.check(); // チェックされていなければチェック
-        saveCustomizer(page); // 変更を保存
+        await saveCustomizer(page); // 変更を保存
     }
 
 }
@@ -42,9 +42,16 @@ async function setSiteType(page, siteType = 'エレガント') {
 
 async function saveCustomizer(page) {
     const saveBtn = page.locator('#save');
-    await saveBtn.click();
-    await expect(saveBtn).toHaveAttribute('value', '公開済み');
-    await expect(saveBtn).toBeDisabled();
+    await expect(saveBtn).toBeVisible();
+    if (await saveBtn.isEnabled()) {
+        await saveBtn.click();
+        await expect(saveBtn).toHaveAttribute('value', '公開済み');
+        await expect(saveBtn).toBeDisabled();
+    } else {
+        // すでに公開済みの場合は何もしない
+        await expect(saveBtn).toHaveAttribute('value', '公開済み');
+        await expect(saveBtn).toBeDisabled();
+    }
 }
 
 
@@ -95,18 +102,27 @@ async function verifyCategoryNavi(
 
     let grid = page.locator(gridSelector);
     await expect(grid).toBeVisible();
-    let count = await grid.locator('.grid-item').count();
+    let count = await grid.locator('.category-item').count();
     await expect(count).toBe(ExpectedCount);
-    let firstTitle = await grid.locator('.grid-item').nth(buttonIndex).locator('h2').innerText();
+    let firstTitle = await grid
+        .locator('.category-item')
+        .nth(0)
+        .locator('.category-link')
+        .innerText();
     expect(firstTitle.trim()).toContain(expectedTitle);
 
-    const secondPageBtn = page.locator(pagenaviSelector).nth(1);
-    await secondPageBtn.click();
-    await expect(grid).toBeVisible();
-    count = await grid.locator('.grid-item').count();
-    await expect(count).toBe(ExpectedCount);
-    firstTitle = await grid.locator('.grid-item').nth(buttonIndex).locator('h2').innerText();
-    expect(firstTitle.trim()).toContain(expectedTitle2);
+    /* カテゴリーを押下して遷移したページが正しいことを確認する*/
+    await grid.locator('.category-item .category-link').first().click();
+    // ページの読み込み完了を待つ
+    await page.waitForLoadState('domcontentloaded');
+
+    // h1テキストを取得
+    const heading = await page.locator('h1').innerText();
+    expect(heading.trim()).toContain(expectedTitle2);
+
+
+    console.log(heading);
+
 
 }
 
@@ -150,33 +166,30 @@ async function verifyLoadMoreGeneric(
 }
 
 
-// test.describe('e2e-home1-PC:', () => {
-
-//     // テスト本体
-//     test('ナビゲーションで次のページに遷移できること', async ({ page }) => {
-
-//         const CONFIG = {
-//             baseUrl: 'https://wpdev.toshidayurika.com',
-
-//         };
-
-//         const {
-//             baseUrl,
-//         } = CONFIG;
-
-//         await test.step('1. 管理画面にログイン', () => login(page, baseUrl));
-//         await test.step('2. カスタマイザー画面を開く', () => openCustomizer(page, baseUrl));
-//         await test.step('3. ホームタイプ設定を開く', () => setSiteType(page, 'エレガント'));
-
-//         // 既存の呼び出し部分はこう書き換え可能
-//         await test.step('4. 新着情報の確認', () =>
-//             verifyPageNavigation(page, baseUrl, '.post-grid', '.page-numbers', 10, 'TEST1', 'サイドFIRE｜【体験談】夫婦でサイドFIRE'));
-
-
-//     });
-// });
-
 test.describe('e2e-home1-PC:', () => {
+
+    // テスト本体
+    test('ナビゲーションで次のページに遷移できること', async ({ page }) => {
+
+        const CONFIG = {
+            baseUrl: 'https://wpdev.toshidayurika.com',
+
+        };
+
+        const {
+            baseUrl,
+        } = CONFIG;
+
+        await test.step('1. 管理画面にログイン', () => login(page, baseUrl));
+        await test.step('2. カスタマイザー画面を開く', () => openCustomizer(page, baseUrl));
+        await test.step('3. ホームタイプ設定を開く', () => setSiteType(page, 'エレガント'));
+
+        // 既存の呼び出し部分はこう書き換え可能
+        await test.step('4. 新着情報の確認', () =>
+            verifyPageNavigation(page, baseUrl, '.post-grid', '.page-numbers', 10, 'TEST1', 'サイドFIRE｜【体験談】夫婦でサイドFIRE'));
+
+
+    });
 
     // テスト本体
     test('カテゴリーナビゲーションでカテゴリーページに遷移できること', async ({ page }) => {
@@ -196,42 +209,44 @@ test.describe('e2e-home1-PC:', () => {
 
         // 既存の呼び出し部分はこう書き換え可能
         await test.step('4. カテゴリーナビゲーションの確認', () =>
-            verifyCategoryNavi(page, baseUrl, '.post-grid', '.page-numbers', 10, 'TEST1', 'サイドFIRE｜【体験談】夫婦でサイドFIRE'));
+            verifyCategoryNavi(page, baseUrl, '.category-list', '.page-numbers', 3, 'FIRE・資産運用', 'カテゴリー: FIRE・資産運用'));
 
 
     });
+
 });
 
 
-// test.describe('e2e-home2-PC:', () => {
 
-//     // テスト本体
-//     test('もっとみるボタンでカードを取得できること', async ({ page }) => {
+test.describe('e2e-home2-PC:', () => {
 
-//         const CONFIG = {
-//             baseUrl: 'https://wpdev.toshidayurika.com',
+    // テスト本体
+    test('もっとみるボタンでカードを取得できること', async ({ page }) => {
 
-//         };
+        const CONFIG = {
+            baseUrl: 'https://wpdev.toshidayurika.com',
 
-//         const {
-//             baseUrl,
-//         } = CONFIG;
+        };
 
-//         await test.step('1. 管理画面にログイン', () => login(page, baseUrl));
-//         await test.step('2. カスタマイザー画面を開く', () => openCustomizer(page, baseUrl));
-//         await test.step('3. ホームタイプ設定を開く', () => setSiteType(page, 'ポップ'));
-//         await test.step('4. 公開ボタンをクリックして変更を保存', () =>
-//             saveCustomizer(page));
+        const {
+            baseUrl,
+        } = CONFIG;
 
-//         // 既存の呼び出し部分はこう書き換え可能
-//         await test.step('5. 新着情報の確認', () =>
-//             verifyLoadMoreGeneric(page, baseUrl, '#latest-posts-grid', '#load-more', 4, 8, 12, 'サイドFIRE｜【体験談】筆者が資産7500万・'));
+        await test.step('1. 管理画面にログイン', () => login(page, baseUrl));
+        await test.step('2. カスタマイザー画面を開く', () => openCustomizer(page, baseUrl));
+        await test.step('3. ホームタイプ設定を開く', () => setSiteType(page, 'ポップ'));
+        await test.step('4. 公開ボタンをクリックして変更を保存', () =>
+            saveCustomizer(page));
 
-//         await test.step('6. カテゴリ情報の確認', () =>
-//             verifyLoadMoreGeneric(page, baseUrl, '.category-posts', '.load-more-cat', 2, 4, 6, 'プラグインテスト', 0, 0));
+        // 既存の呼び出し部分はこう書き換え可能
+        await test.step('5. 新着情報の確認', () =>
+            verifyLoadMoreGeneric(page, baseUrl, '#latest-posts-grid', '#load-more', 4, 8, 12, 'サイドFIRE｜【体験談】筆者が資産7500万・'));
 
-//         await test.step('7. カテゴリ情報の3つ目の確認', () =>
-//             verifyLoadMoreGeneric(page, baseUrl, '.category-posts', '.load-more-cat', 2, 4, 6, '節約｜【夫婦実録】月35万円', 2, 2));
+        await test.step('6. カテゴリ情報の確認', () =>
+            verifyLoadMoreGeneric(page, baseUrl, '.category-posts', '.load-more-cat', 2, 4, 6, 'プラグインテスト', 0, 0));
 
-//     });
-// });
+        await test.step('7. カテゴリ情報の3つ目の確認', () =>
+            verifyLoadMoreGeneric(page, baseUrl, '.category-posts', '.load-more-cat', 2, 4, 6, '節約｜【夫婦実録】月35万円', 2, 2));
+
+    });
+});
