da7a3e792f490952e36a20531f2c0b592341e47f
data2coordi <data2coordi@gmail.com>
2025-08-16 09:32:35 +0900

    playwrightタイムアウト変更

--- tests/e2e-tests/playwright.config.js
+++ tests/e2e-tests/playwright.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+    timeout: 120_000, // 各テストのデフォルトタイムアウト（ms）
     use: {
         video: 'on',  // テスト開始から終了まで常に動画を録画する
+        actionTimeout: 3_000, // click や fill など1アクションのタイムアウト
         // video: 'retain-on-failure', // 失敗時のみ保存したい場合はこちら
     },
 });
