1c1753c36a9b14e6cf51a38820bd450a7c7a461e
data2coordi <data2coordi@gmail.com>
2025-08-24 08:58:44 +0900

    スナップショット修正

--- tests/e2e-tests/tests/visual.spec.js
+++ tests/e2e-tests/tests/visual.spec.js
@@ -48,45 +48,48 @@ const siteTypes = ['エレガント', 'ポップ'];
 
 // ======= テスト展開 =======
 for (const siteType of siteTypes) {
-  test.beforeAll(async ({ browser }) => {
-    const page = await browser.newPage();
-    await openCustomizer(page);
-    await setSiteType(page, siteType);
-    await ensureCustomizerRoot(page);
-    await selSliderEffect(page, 'スライド', '60'); // スライダーエフェクトを「スライド」、変更時間間隔を3秒に設定
-    await saveCustomizer(page);
+  test.describe(`${siteType}`, () => {
+    test.beforeAll(async ({ browser }) => {
+      const page = await browser.newPage();
+      await openCustomizer(page);
+      await setSiteType(page, siteType);
+      await ensureCustomizerRoot(page);
+      await selSliderEffect(page, 'スライド', '60'); // スライダーエフェクトを「スライド」、変更時間間隔を3秒に設定
+      await saveCustomizer(page);
 
-    await page.close();
+      await page.close();
+
+    });
+
+    for (const device of devices) {
+
+      test.describe(`${device.name} : ${siteType}`, () => {
+        test.use(device.use);
+
+
+        for (const { name, url, options } of pages) {
+          test(`： ${name}`, async ({ page }) => {
+            await page.goto(url, { waitUntil: 'networkidle' });
+
+            // 安全待機: ページ全体が見える状態を確認
+            //await page.locator('body').waitFor({ state: 'visible', timeout: 10000 });
+
+            // 任意で、LazyLoad画像の読み込みやフォント描画を待つ場合
+            //await page.waitForTimeout(500); // 0.5秒程度の余裕待機
+
+            const options = {
+              maxDiffPixelRatio: 0.02, // 人間の目でわからないレベル
+              threshold: 0.01
+            };
+            await expect(page).toHaveScreenshot({ fullPage: true, timeout: 100000, ...options });
+          });
+          //break;
+
+        }
+      });
+      //break;
+    }
 
   });
-
-  for (const device of devices) {
-
-    test.describe(`${device.name} : ${siteType}`, () => {
-      test.use(device.use);
-
-
-      for (const { name, url, options } of pages) {
-        test(`： ${name}`, async ({ page }) => {
-          await page.goto(url, { waitUntil: 'networkidle' });
-
-          // 安全待機: ページ全体が見える状態を確認
-          //await page.locator('body').waitFor({ state: 'visible', timeout: 10000 });
-
-          // 任意で、LazyLoad画像の読み込みやフォント描画を待つ場合
-          //await page.waitForTimeout(500); // 0.5秒程度の余裕待機
-
-          const options = {
-            maxDiffPixelRatio: 0.02, // 人間の目でわからないレベル
-            threshold: 0.01
-          };
-          await expect(page).toHaveScreenshot({ fullPage: true, timeout: 100000, ...options });
-        });
-        break;
-
-      }
-    });
-    break;
-  }
-  break;
+  //break;
 }
