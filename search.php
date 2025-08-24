b60de61821b604fc93fa0b2c440dad05ea65815e
data2coordi <data2coordi@gmail.com>
2025-08-24 10:09:54 +0900

    検索結果画面のレイアウト修正

--- search.php
+++ search.php
@@ -1,53 +1,56 @@
-<?php
-/**
- * The template for displaying search results pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package Integlight
- */
+	<?php
+	/**
+	 * The template for displaying search results pages
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+	 *
+	 * @package Integlight
+	 */
 
-get_header();
-?>
+	get_header();
+	?>
+	<div class="ly_site_content">
 
-	<main id="primary" class="site-main">
+		<main id="primary" class="site-main  ly_site_content_main">
 
-		<?php if ( have_posts() ) : ?>
+			<?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'integlight' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+				<header class="page-header">
+					<h1 class="page-title">
+						<?php
+						/* translators: %s: search query. */
+						printf(esc_html__('Search Results for: %s', 'integlight'), '<span>' . get_search_query() . '</span>');
+						?>
+					</h1>
+				</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				/* Start the Loop */
+				while (have_posts()) :
+					the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part('template-parts/content', 'arc');
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part('template-parts/content', 'arc');
 
-			endwhile;
+				endwhile;
 
-			the_posts_navigation();
+				the_posts_navigation();
 
-		else :
+			else :
 
-			get_template_part( 'template-parts/content', 'none' );
+				get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
+			endif;
+			?>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
 
-<?php
-get_sidebar();
-get_footer();
+
+
+		<?php get_sidebar(); ?>
+	</div> <!-- site-content-->
+	<?php get_footer(); ?>
