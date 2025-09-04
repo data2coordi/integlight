7470941bcaf3dfb3da3a4439fea236e88b2628cf
data2coordi <data2coordi@gmail.com>
2025-09-05 08:52:54 +0900

    調整

--- tests/e2e-tests/tests/visual.spec.js
+++ tests/e2e-tests/tests/visual.spec.js
@@ -17,7 +17,13 @@ import {
 const baseUrl = 'https://wpdev.auroralab-design.com';
 
 const pages = [
-  { name: 'カテゴリ一覧', url: `${baseUrl}/category/fire-blog/` }
+  { name: 'home top', url: `${baseUrl}/` },
+  { name: 'front top', url: `${baseUrl}/fire%e3%81%a7%e8%87%aa%e7%94%b1%e3%81%a8%e6%88%90%e9%95%b7%e3%82%92%e6%8e%b4%e3%82%80%ef%bc%81/` },
+  { name: 'カテゴリ一覧', url: `${baseUrl}/category/fire-blog/` },
+  { name: '固定ページ', url: `${baseUrl}/profile/` },
+  { name: 'ブログ', url: `${baseUrl}/sidefire-7500man-life-cost/` },
+  { name: 'プラグイン1', url: `${baseUrl}/ptest/` },
+  { name: 'プラグイン2', url: `${baseUrl}/ptest2/` }
 ];
 
 const devices = [
@@ -28,6 +34,14 @@ const devices = [
       userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137 Safari/537.36'
     }
   },
+  {
+    name: 'Mobile',
+    use: {
+      viewport: { width: 375, height: 800 },
+      userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1',
+      extraHTTPHeaders: { 'sec-ch-ua-mobile': '?1' }
+    }
+  }
 ];
 
 const siteTypes = ['エレガント', 'ポップ'];
