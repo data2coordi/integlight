81367e4b49cb0bb593ac82130baaf09b6393032c
data2coordi <data2coordi@gmail.com>
2025-09-07 16:56:43 +0900

    背景色のグラデーション

--- home.php
+++ home.php
@@ -4,16 +4,17 @@
 if (is_front_page() != false) {
     integlight_display_headerContents();
 }
+$home_type = get_theme_mod('integlight_hometype_setting', 'home1');
 ?>
-<main id="primary">
 
+<main id="primary" class=<?php echo $home_type; ?>>
+
+    >
     <?php
-    $home_type = get_theme_mod('integlight_hometype_setting', 'home1');
 
     $cache = new Integlight_Cache_TemplatePart();
     $key = 'home_content_' . $home_type;
     $cache->displayTemplatePart($key, 'template-parts/content-home', $home_type);
-
 
     ?>
 
