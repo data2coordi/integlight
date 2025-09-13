693b71862eab63e5276da0cc821ab072ffd2d299
data2coordi <data2coordi@gmail.com>
2025-09-13 12:18:13 +0900

    criticalcss

--- inc/integlight-functions-pf.php
+++ inc/integlight-functions-pf.php
@@ -812,7 +812,7 @@ function aurora_add_critical_css()
         echo "<style id='critical-css'>{$critical_css}</style>";
     }
 }
-add_action('wp_head', 'aurora_add_critical_css', 1); // 優先度1で早く出力
+//add_action('wp_head', 'aurora_add_critical_css', 1); // 優先度1で早く出力
 /********************************************************************/
 /* critical css のロード e 	*/
 /********************************************************************/
