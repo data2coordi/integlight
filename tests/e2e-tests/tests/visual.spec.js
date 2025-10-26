2d83d8d29b193a7a34631453ee2e5047abd7648d
data2coordi <data2coordi@gmail.com>
2025-10-26 19:03:59 +0900

    元の状態

--- tests/e2e-tests/tests/visual.spec.js
+++ tests/e2e-tests/tests/visual.spec.js
@@ -54,22 +54,19 @@ const siteTypes = ["エレガント", "ポップ"];
 // ======= テスト展開 =======
 for (const siteType of siteTypes) {
   test.describe(`${siteType}`, () => {
-    test.describe.serial(`サーバー設定変更シリアル`, () => {
-      test(`サーバー設定：${siteType}`, async ({ browser }) => {
-        const page = await browser.newPage();
-        await openCustomizer(page);
-        await setSiteType(page, siteType);
-        await ensureCustomizerRoot(page);
-        await selSliderEffect(page, "スライド", "60"); // スライダーエフェクトを「スライド」、変更時間間隔を3秒に設定
-        await saveCustomizer(page);
+    test.beforeAll(async ({ browser }) => {
+      const page = await browser.newPage();
+      await openCustomizer(page);
+      await setSiteType(page, siteType);
+      await ensureCustomizerRoot(page);
+      await selSliderEffect(page, "スライド", "60"); // スライダーエフェクトを「スライド」、変更時間間隔を3秒に設定
+      await saveCustomizer(page);
 
-        await page.close();
-      });
+      await page.close();
     });
 
     for (const device of devices) {
-      test.describe
-        .parallel(`検証のパラレル実行：${device.name} : ${siteType}`, () => {
+      test.describe(`${device.name} : ${siteType}`, () => {
         test.use(device.use);
 
         for (const { name, url, options } of pages) {
