aef10a199da030ac64549bcd6fc4866152a9a73d
data2coordi <data2coordi@gmail.com>
2025-10-28 09:27:28 +0900

    a

--- tests/e2e-tests/tests/visual.spec.js
+++ tests/e2e-tests/tests/visual.spec.js
@@ -14,15 +14,7 @@ const baseUrl = "https://wpdev.auroralab-design.com";
 
 const pages = [
   { name: "home top", url: `${baseUrl}/` },
-  {
-    name: "front top",
-    url: `${baseUrl}/fire%e3%81%a7%e8%87%aa%e7%94%b1%e3%81%a8%e6%88%90%e9%95%b7%e3%82%92%e6%8e%b4%e3%82%80%ef%bc%81/`,
-  },
-  { name: "カテゴリ一覧", url: `${baseUrl}/category/fire-blog/` },
-  { name: "固定ページ", url: `${baseUrl}/profile/` },
   { name: "ブログ", url: `${baseUrl}/sidefire-7500man-life-cost/` },
-  { name: "プラグイン1", url: `${baseUrl}/ptest/` },
-  { name: "プラグイン2", url: `${baseUrl}/ptest2/` },
 ];
 
 const pagesForImageHeader = [
@@ -49,7 +41,7 @@ const devices = [
   },
 ];
 
-const siteTypes = ["エレガント", "ポップ"];
+const siteTypes = ["エレガント"];
 
 // ======= テスト展開 =======
 for (const siteType of siteTypes) {
@@ -96,41 +88,4 @@ for (const siteType of siteTypes) {
     }
   });
   //break;
-}
-
-for (const siteType of siteTypes) {
-  test.describe(`画像ヘッダーテスト:${siteType}`, () => {
-    test.beforeAll(async ({ browser }) => {
-      const page = await browser.newPage();
-      await openCustomizer(page);
-      await setSiteType(page, siteType);
-      await ensureCustomizerRoot(page);
-      await openHeaderSetting(page, "静止画像");
-      await saveCustomizer(page);
-
-      await page.close();
-    });
-
-    for (const device of devices) {
-      test.describe(`${device.name} : ${siteType}`, () => {
-        test.use(device.use);
-
-        for (const { name, url, options } of pagesForImageHeader) {
-          test(`： ${name}`, async ({ page }) => {
-            await page.goto(url, { waitUntil: "networkidle" });
-
-            const options = {
-              maxDiffPixelRatio: 0.03, // 人間の目でわからないレベル
-              threshold: 0.03,
-            };
-            await expect(page).toHaveScreenshot({
-              fullPage: true,
-              timeout: 100000,
-              ...options,
-            });
-          });
-        }
-      });
-    }
-  });
 }
