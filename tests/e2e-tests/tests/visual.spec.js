8fd5577fabc43bf8dd8840a264d801c385e2035a
data2coordi <data2coordi@gmail.com>
2025-10-28 10:19:05 +0900

    a

--- tests/e2e-tests/tests/visual.spec.js
+++ tests/e2e-tests/tests/visual.spec.js
@@ -1,12 +1,4 @@
 import { test, expect } from "@playwright/test";
-import {
-  openCustomizer,
-  saveCustomizer,
-  setSiteType,
-  selSliderEffect,
-  ensureCustomizerRoot,
-  openHeaderSetting,
-} from "../utils/common";
 // ======= 共通関数 =======
 
 // ======= 設定 =======
@@ -43,18 +35,6 @@ const devices = [
 
 // ======= テスト展開 =======
 test.describe("ビジュアルテスト", () => {
-  test.beforeAll(async ({ browser }, testInfo) => {
-    const { siteType } = testInfo.project.use; // ✅ ここで取得
-    const page = await browser.newPage();
-    await openCustomizer(page);
-    await setSiteType(page, siteType);
-    await ensureCustomizerRoot(page);
-    await selSliderEffect(page, "スライド", "60"); // スライダーエフェクトを「スライド」、変更時間間隔を3秒に設定
-    await saveCustomizer(page);
-
-    await page.close();
-  });
-
   for (const device of devices) {
     test.describe(`${device.name}`, () => {
       test.use(device.use);
