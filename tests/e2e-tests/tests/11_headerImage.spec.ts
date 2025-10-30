48163421a26d2a7ab0eceb3355f5d0dbab7e0f38
data2coordi <data2coordi@gmail.com>
2025-10-30 14:06:50 +0900

    a

--- tests/e2e-tests/tests/11_headerImage.spec.ts
+++ tests/e2e-tests/tests/11_headerImage.spec.ts
@@ -26,65 +26,6 @@ const TEST_CONFIGS = {
 /****設定用 ***********************************/
 /****設定用 ***********************************/
 
-// ヘッダー設定をヘッダー画像に変更
-async function openHeaderImage(page) {
-  await page.getByRole("button", { name: "ヘッダー設定" }).click();
-  await page.getByRole("button", { name: "2.静止画像設定" }).click();
-}
-
-// ヘッダー画像テキスト設定
-async function setHeaderImageText(page, config) {
-  await openHeaderImage(page);
-
-  // テキスト設定
-  await page
-    .getByLabel("ヘッダー画像メインテキスト")
-    .nth(0)
-    .fill(config.mainText);
-  await page.getByLabel("ヘッダー画像サブテキスト").nth(0).fill(config.subText);
-
-  await expect(
-    page.getByLabel("ヘッダー画像メインテキスト").nth(0)
-  ).toHaveValue(config.mainText);
-  await expect(page.getByLabel("ヘッダー画像サブテキスト").nth(0)).toHaveValue(
-    config.subText
-  );
-
-  // テキストカラー設定
-  await page.getByRole("button", { name: "色を選択" }).click();
-  const input = page.getByLabel("ヘッダー画像テキストの色");
-  await input.fill(config.textColor);
-
-  // フォント設定
-  const label = page.locator("label", {
-    hasText: "ヘッダー画像テキストのフォント",
-  });
-  const selectId = await label.getAttribute("for");
-  if (!selectId)
-    throw new Error(
-      `ラベル "ヘッダー画像テキストのフォント" に対応する select が見つかりません`
-    );
-  const select = page.locator(`#${selectId}`);
-  await select.waitFor({ state: "visible" });
-  await select.selectOption(config.textFont);
-
-  // テキスト位置設定
-  await page
-    .getByLabel("ヘッダー画像テキストの上位置（px）")
-    .fill(config.textPositionTop);
-  await page
-    .getByLabel("ヘッダー画像テキストの左位置（px）")
-    .fill(config.textPositionLeft);
-
-  // テキスト位置設定
-  await page
-    .getByLabel("ヘッダー画像テキストのモバイル上位置（px）")
-    .fill(config.textPositionTop_mobile);
-  await page
-    .getByLabel("ヘッダー画像テキストのモバイル左位置（px）")
-    .fill(config.textPositionLeft_mobile);
-}
-
 // 共通テストフロー
 async function setHeaderImageDetailSettings(page, config) {
   const keyValue = {
@@ -103,16 +44,6 @@ async function setHeaderImageDetailSettings(page, config) {
   };
   const cm_manager = new Customizer_manager(page);
   await cm_manager.apply(keyValue);
-
-  // await test.step("カスタマイザー画面を開く", () => openCustomizer(page));
-  // await test.step("ヘッダー画像テキストを設定する", () =>
-  //   setHeaderImageText(page, config));
-
-  // // await ensureCustomizerRoot(page);
-  // // await test.step("ヘッダー画像を設定する", () => setHeaderImage(page, config));
-
-  // await test.step("公開ボタンをクリックして変更を保存", () =>
-  //   saveCustomizer(page));
 }
 
 /****検証用 ***********************************/
