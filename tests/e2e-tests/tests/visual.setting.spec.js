35ed481fbdb8902b6c8dc25faf2df974404584f2
data2coordi <data2coordi@gmail.com>
2025-11-01 12:22:21 +0900

    aあ

--- tests/e2e-tests/tests/visual.setting.spec.js
+++ tests/e2e-tests/tests/visual.setting.spec.js
@@ -5,14 +5,16 @@ import { test } from "@playwright/test";
 // ======= テスト展開 =======
 test.describe("ビジュアルテスト", () => {
   //test("カスタマイザー設定", async ({ browser }, testInfo) => {
-  test("カスタマイザー設定", async ({ browser }) => {
+  test("カスタマイザー設定", async ({ page, browser }) => {
     //console.log(`Running test in project: ${testInfo.project.name}`);
 
     //let keyValue = testInfo.project.use.keyValue;
     //console.log(`@@@@@keyValue@@@@@: ${JSON.stringify(keyValue)}`);
 
-    const page = await browser.newPage();
+    //const page = await browser.newPage();
     await page.goto("/", { waitUntil: "domcontentloaded" });
+
+    await expect(page).toHaveURL(/.*/); // URLが何かあればOK
     //const cm_manager = new Customizer_manager(page);
     //await cm_manager.apply(keyValue);
     await page.waitForTimeout(2000);
