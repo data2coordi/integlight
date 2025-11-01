997292d13d97e12a53025a90abbedf4696eed825
data2coordi <data2coordi@gmail.com>
2025-11-02 08:46:21 +0900

    a

---
+++ tests/e2e-tests/playwright.config.visual.js
@@ -0,0 +1,174 @@
+import { defineConfig, devices } from "@playwright/test";
+
+// 環境によってURLや認証ファイルパスが変わるため、定数として定義
+const BASE_URL = "https://wpdev.auroralab-design.com";
+const authFile = "playwright/.auth/user.json";
+
+const visualInitTestCnf = [
+  {
+    testid: "elegant_slider",
+    siteType: "エレガント",
+    headerType: "スライダー",
+    sliderType: {
+      effect: "スライド",
+      interval: "60",
+    },
+    sliderImg: { imagePartialName: "Firefly-1498.webp" },
+    sliderText: {
+      mainText: "visual init slider main テストタイトル",
+      subText: "visual init slider sub テストタイトル",
+      top: "10",
+      left: "10",
+      deviceType: "PC",
+      textColor: "#0000ff",
+      textFont: "yu_mincho",
+    },
+  },
+  {
+    testid: "pop_slider",
+    siteType: "ポップ",
+    headerType: "スライダー",
+    sliderType: { effect: "スライド", interval: "60" },
+  },
+  {
+    testid: "pop_img",
+    siteType: "ポップ",
+    headerType: "静止画像",
+    headerImageImg: { imageName: "Firefly-1498.webp" },
+    headerImageText: {
+      mainText: "テストタイトル",
+      subText: "これはPlaywrightテストによって入力された説明文です。",
+      textFont: "yu_mincho",
+      textPositionTop: "20",
+      textPositionLeft: "30",
+      textPositionTop_mobile: "5",
+      textPositionLeft_mobile: "10",
+      textColor: "#ff0000",
+    },
+  },
+];
+
+const visualInitProjects = visualInitTestCnf.flatMap(
+  ({ testid, siteType, headerType, sliderType }) => [
+    {
+      name: `setting_init_${testid}`,
+      testDir: "./tests",
+      testMatch: [/visual\.setting\.spec\.js/],
+      dependencies: ["setup_init"],
+      use: {
+        baseURL: "https://t2.auroralab-design.com",
+        ...devices["Desktop Chrome"],
+        storageState: authFile,
+        keyValue: {
+          testid,
+          siteType,
+          headerType,
+          sliderType,
+        },
+      },
+    },
+    {
+      name: `visual_init_${testid}`,
+      testDir: "./tests",
+      snapshotDir: "./tests/visual.init/", // 期待値（比較元）画像
+      testMatch: [/visual\.spec\.js/],
+      dependencies: [`setting_init_${testid}`],
+      use: {
+        baseURL: "https://t2.auroralab-design.com",
+        ...devices["Desktop Chrome"],
+        storageState: authFile,
+        keyValue: {
+          testid,
+          siteType,
+          headerType,
+        },
+      },
+      workers: 4,
+    },
+  ]
+);
+
+const visualTestCnf = [
+  {
+    testid: "elegant_slider",
+    siteType: "エレガント",
+    headerType: "スライダー",
+    sliderType: { effect: "スライド", interval: "60" },
+  },
+  {
+    testid: "pop_slider",
+    siteType: "ポップ",
+    headerType: "スライダー",
+    sliderType: { effect: "スライド", interval: "60" },
+  },
+  {
+    testid: "pop_img",
+    siteType: "ポップ",
+    headerType: "静止画像",
+  },
+];
+
+const visualProjects = visualTestCnf.flatMap((conf) => [
+  {
+    name: `setting_${conf.testid}`,
+    testDir: "./tests",
+    testMatch: [/visual\.setting\.spec\.js/],
+    dependencies: ["setup"],
+    use: {
+      ...devices["Desktop Chrome"],
+      storageState: authFile,
+      keyValue: conf,
+    },
+  },
+  {
+    name: `visual_${conf.testid}`,
+    testDir: "./tests",
+    testMatch: [/visual\.spec\.js/],
+    dependencies: [`setting_${conf.testid}`],
+    use: {
+      ...devices["Desktop Chrome"],
+    },
+    workers: 4,
+  },
+]);
+
+export default defineConfig({
+  reporter: [
+    ["list"],
+    [
+      "html",
+      {
+        open: "never",
+        outputFolder: "test-results",
+      },
+    ],
+  ],
+  // 各テストのデフォルトタイムアウト（ms）
+  timeout: 60_000,
+
+  // プロジェクト間で共有される設定
+  use: {
+    // click や fill など1アクションのタイムアウト
+    actionTimeout: 10_000,
+    // 動画録画設定
+    video: "off",
+    // ブラウザのベースURL
+    baseURL: BASE_URL,
+  },
+
+  // 複数のテストプロジェクトを定義
+  projects: [
+    // 1. 認証処理を実行するプロジェクト
+    {
+      name: "setup",
+      testMatch: "auth.setup.ts",
+    },
+    {
+      name: "setup_init",
+      testMatch: "auth.setup.ts",
+      baseURL: "https://t2.auroralab-design.com",
+    },
+    ...visualProjects,
+    ...visualInitProjects,
+  ],
+});
