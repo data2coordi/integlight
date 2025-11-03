1fdea88b6995ed7b32fea025242eea77e23a19fa
data2coordi <data2coordi@gmail.com>
2025-11-03 09:59:29 +0900

    a

--- tests/e2e-tests/playwright.config.factory.js
+++ tests/e2e-tests/playwright.config.factory.js
@@ -40,6 +40,7 @@ export function createVisualConfig({
       dependencies: [`${projectPrefix}setting_${conf.testid}`],
       use: {
         ...devices["Desktop Chrome"],
+        storageState: authFile,
       },
       workers: 1,
     },
