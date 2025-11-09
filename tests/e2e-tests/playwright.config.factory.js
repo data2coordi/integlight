06d94cda2cee033c009ae93b8c80cfa05ceac33d
data2coordi <data2coordi@gmail.com>
2025-11-09 19:20:07 +0900

    ビジュアルテストでページを指定可能に変更

--- tests/e2e-tests/playwright.config.factory.js
+++ tests/e2e-tests/playwright.config.factory.js
@@ -13,13 +13,13 @@ const authFile = "playwright/.auth/user.json";
  */
 
 const pages = [
-  { name: "home top", url: `${baseUrl}/` },
+  { name: "home top", url: "/" },
   {
     name: "front top",
-    url: `${baseUrl}/fire%e3%81%a7%e8%87%aa%e7%94%b1%e3%81%a8%e6%88%90%e9%95%b7%e3%82%92%e6%8e%b4%e3%82%80%ef%bc%81/`,
+    url: "/fire%e3%81%a7%e8%87%aa%e7%94%b1%e3%81%a8%e6%88%90%e9%95%b7%e3%82%92%e6%8e%b4%e3%82%80%ef%bc%81/",
   },
-  { name: "カテゴリ一覧", url: `${baseUrl}/category/fire-blog/` },
-  { name: "固定ページ", url: `${baseUrl}/profile/` },
+  { name: "カテゴリ一覧", url: "/category/fire-blog/" },
+  { name: "固定ページ", url: "/profile/" },
 ];
 
 export function createVisualConfig({
