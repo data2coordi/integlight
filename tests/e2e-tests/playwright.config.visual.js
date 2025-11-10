5e7b04c47312b9d4dcd00a656d1123b66ad27aa1
data2coordi <data2coordi@gmail.com>
2025-11-10 11:27:09 +0900

    簡単セットアップのテスト道入

--- tests/e2e-tests/playwright.config.visual.js
+++ tests/e2e-tests/playwright.config.visual.js
@@ -5,20 +5,26 @@ const BASE_URL = "https://wpdev.auroralab-design.com";
 const visualTestCnf = [
   {
     testid: "elegant_slider",
-    siteType: "エレガント",
-    headerType: "スライダー",
-    sliderType: { effect: "スライド", interval: "60" },
+    customizer: {
+      siteType: "エレガント",
+      headerType: "スライダー",
+      sliderType: { effect: "スライド", interval: "60" },
+    },
   },
   {
     testid: "pop_slider",
-    siteType: "ポップ",
-    headerType: "スライダー",
-    sliderType: { effect: "スライド", interval: "60" },
+    customizer: {
+      siteType: "ポップ",
+      headerType: "スライダー",
+      sliderType: { effect: "スライド", interval: "60" },
+    },
   },
   {
     testid: "pop_img",
-    siteType: "ポップ",
-    headerType: "静止画像",
+    customizer: {
+      siteType: "ポップ",
+      headerType: "静止画像",
+    },
   },
 ];
 
