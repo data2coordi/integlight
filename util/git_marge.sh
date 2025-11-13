44d3606438b517bb0af1d8848616c0aaa07122ce
data2coordi <data2coordi@gmail.com>
2025-11-13 18:16:40 +0900

    スタート地点

--- util/git_marge.sh
+++ util/git_marge.sh
@@ -1,4 +1,19 @@
 
-git checkout デバッグ1
-git merge --squash デバッグ3
-git commit -m "デバッグ3の変更をまとめて反映"
+
+git checkout jmaster
+git pull origin jmaster
+
+git merge --squash jdev
+
+git commit -m "feat: [タスク名] 〇〇機能の完全な実装"
+
+git push origin jmaster
+
+
+# ローカルのdevブランチに戻り、masterをチェックアウトし直す（ブランチ削除のため）
+# リモートのdevブランチを削除
+git push origin :jdev 
+
+# ローカルのdevブランチを削除
+git branch -d jdev
+git checkout -b jdev
